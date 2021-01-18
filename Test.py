dd04417d42b6c56ef1c7dd5fcda55b8383eb3e9b
turcinv <77553864+turcinv@users.noreply.github.com>
2021-01-18 10:01:04 +0100

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,17 @@
+print("Test")
+marks = 0
+questions = 0
+question_1 = print("Question 1: \n A \n B \n C \n D \n")
+answer_1 = input("Answer:")
+if answer_1 == ("A" or "a"):
+    print("Correct")
+    marks = marks + 1
+    questions = questions + 1
+
+else:
+    print("Bad")
+    questions = questions + 1
+
+result = marks / questions
+print("Your result is: ", result * 100, "%")
+input()
