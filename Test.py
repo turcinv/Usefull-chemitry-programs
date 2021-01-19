32016f2f56cf452bc15757011a982899213179c7
Vít Turčin <77553864+turcinv@users.noreply.github.com>
2021-01-19 08:56:26 +0100

    Update Test.py

--- Test.py
+++ Test.py
@@ -3,7 +3,7 @@ marks = 0
 questions = 0
 question_1 = print("Question 1: \n A \n B \n C \n D \n")
 answer_1 = input("Answer:")
-if answer_1 == ("A") or ("a"):
+if answer_1 == (("A") or ("a")):
     print("Correct")
     marks = marks + 1
     questions = questions + 1
