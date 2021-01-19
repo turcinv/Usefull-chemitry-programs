c42de862c1201cbca08fb4ebcd126f8335159283
Vít Turčin <77553864+turcinv@users.noreply.github.com>
2021-01-19 11:37:24 +0100

    Update and rename Test1,1.py to Test1_1.py

--- Test1_1.py
+++ Test1_1.py
@@ -4,23 +4,23 @@ questions = 0
 question_1 = print("Question no.1: What particle is in the electron shell? \n A Electron  \n B Neutron \n C Pozitron \n D Proton\n")
 answer_1 = input("Answer: ")
 if answer_1 == (("A") or ("a")):
-    print("Correct")
+    print("Correct\n")
     marks = marks + 1
     questions = questions + 1
 
 else:
-    print("Bad")
+    print("Bad\n")
     questions = questions + 1
     
 question_2 = print("Question no.2: Which substance is polar? \n A Benzene \n B Pentane \n C Water \n D Ethylacetate \n")
 answer_2 = input("Answer: ")
 if answer_2 == (("C") or ("c")):
-    print("Correct")
+    print("Correct\n")
     marks = marks + 1
     questions = questions + 1
 
 else:
-    print("Bad")
+    print("Bad\n")
     questions = questions + 1
 
 result = marks / questions
